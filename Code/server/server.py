0749218b7ccd0d1620b5bc85639146ca855331a6
tên_github_cua_ban <email_cua_ban@gmail.com>
2026-03-25 22:52:19 +0700

    update

--- Code/server/server.py
+++ Code/server/server.py
@@ -1,4 +1,7 @@
 import socket 
+import threading
+
+
 sever_sock = socket.socket ( socket.AF_INET, socket. SOCK_STREAM)
 sever_sock.bind(('0.0.0.0',9999))
 sever_sock.listen()
@@ -9,8 +12,17 @@ while True :
     print  (f" Có người kết nối từ {address} ")
     client_sock.send(b"kết nối thành cong6 \n" )
     client_sock.close()
-    print (f"{address} đã ngắt kết nối \n")
+    print (f"{address} đã ngắt kết nối \n") 
+      
 
+def xu_ly_client(client_sock):
+    # nói chuyện với 1 client
+    pass
 
+while True:
+    client_sock, addr = server_sock.accept()
+    # tạo thread mới cho mỗi client, chạy song song
+    t = threading.Thread(target=xu_ly_client, args=(client_sock,))
+    t.start()
 
 
