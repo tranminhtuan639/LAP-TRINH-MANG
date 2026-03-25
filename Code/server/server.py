dbfbf8bae991987bdf918dee2653a6fb57857501
tên_github_cua_ban <email_cua_ban@gmail.com>
2026-03-25 21:55:42 +0700

    up

--- Code/server/server.py
+++ Code/server/server.py
@@ -1,1 +1,10 @@
 import socket 
+sever_sock = socket.socket ( socket.AF_INET, socket. SOCK_STREAM)
+sever_sock.bind(('0.0.0.0',9999))
+sever_sock.listen()
+print ("Sever đang chạy chờ xíu ")
+client_sock , address = sever_sock.accept()
+
+
+
+
