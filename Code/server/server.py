046bf232614444cf6cf5973bc5dc8a67e9c68c2d
tên_github_cua_ban <email_cua_ban@gmail.com>
2026-03-25 22:35:18 +0700

    update

--- Code/server/server.py
+++ Code/server/server.py
@@ -3,7 +3,13 @@ sever_sock = socket.socket ( socket.AF_INET, socket. SOCK_STREAM)
 sever_sock.bind(('0.0.0.0',9999))
 sever_sock.listen()
 print ("Sever đang chạy chờ xíu ")
-client_sock , address = sever_sock.accept()
+
+while True :
+    client_sock , address = sever_sock.accept()
+    print  (f" Có người kết nối từ {address} ")
+    client_sock.send(b"kết nối thành cong6 \n" )
+    client_sock.close()
+    print (f"{address} đã ngắt kết nối \n")
 
 
 
